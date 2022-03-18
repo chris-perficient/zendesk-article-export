abdd4b5dc614849902cae480edd42698316bd2b3
chris-perficient <christopher.mcardle@perficient.com>
2022-03-18 11:51:29 -0500

    consolidated down to one function

--- exportArticles.js
+++ exportArticles.js
@@ -14,8 +14,14 @@ const encoded = Buffer.from(
 ).toString("base64");
 
 let log = [];
+let brands = [
+  `https://prft-mso.zendesk.com/api/v2/help_center/en-us/articles`,
+  `https://prft-converge.zendesk.com/api/v2/help_center/en-us/articles`,
+  `https://prftamazonconnect.zendesk.com/api/v2/help_center/en-us/articles`,
+  `https://prft-cconnect.zendesk.com/api/v2/help_center/en-us/articles`,
+];
 
-async function exportTwilioFlex() {
+async function exportArticles() {
   let url = `https://perficient.zendesk.com/api/v2/help_center/en-us/articles`;
   let response;
   while (url) {
@@ -34,110 +40,21 @@ async function exportTwilioFlex() {
     for (const article of data["articles"]) {
       log.push(article);
     }
-    url = data.next_page;
+
+    if (data.next_page != null) {
+      console.log("not null", data.next_page);
+      url = data.next_page;
+    } else {
+      let temp = brands.shift();
+      url = temp;
+      console.log("Brand", url);
+    }
   }
-  exportAmazonConnect();
-}
-
-async function exportAmazonConnect() {
-  let url = `https://prftamazonconnect.zendesk.com/api/v2/help_center/en-us/articles`;
-  let response;
-  while (url) {
-    response = await axios.get(`${url}`, {
-      headers: { Authorization: `Basic ${encoded}` },
-    });
-
-    if (response.status != 200) {
-      console.log(
-        `Failed to retrieve Amazon Connect articles with error ${response.status}`
-      );
-      return;
-    }
-
-    let data = response.data;
-    for (const article of data["articles"]) {
-      log.push(article);
-    }
-    url = data.next_page;
-  }
-  exportClarityConnect();
-}
-
-async function exportClarityConnect() {
-  let url = `https://prft-cconnect.zendesk.com/api/v2/help_center/en-us/articles`;
-  let response;
-  while (url) {
-    response = await axios.get(`${url}`, {
-      headers: { Authorization: `Basic ${encoded}` },
-    });
-
-    if (response.status != 200) {
-      console.log(
-        `Failed to retrieve Clarity Connect articles with error ${response.status}`
-      );
-      return;
-    }
-
-    let data = response.data;
-    for (const article of data["articles"]) {
-      log.push(article);
-    }
-    url = data.next_page;
-  }
-  exportConverge();
-}
-
-async function exportConverge() {
-  let url = `https://prft-converge.zendesk.com/api/v2/help_center/en-us/articles`;
-  let response;
-  while (url) {
-    response = await axios.get(`${url}`, {
-      headers: { Authorization: `Basic ${encoded}` },
-    });
-
-    if (response.status != 200) {
-      console.log(
-        `Failed to retrieve Converge articles with error ${response.status}`
-      );
-      return;
-    }
-
-    let data = response.data;
-    for (const article of data["articles"]) {
-      log.push(article);
-    }
-    url = data.next_page;
-  }
-  exportMso();
-}
-
-async function exportMso() {
-  let url = `https://prft-mso.zendesk.com/api/v2/help_center/en-us/articles`;
-  let response;
-  while (url) {
-    response = await axios.get(`${url}`, {
-      headers: { Authorization: `Basic ${encoded}` },
-    });
-
-    if (response.status != 200) {
-      console.log(
-        `Failed to retrieve MSO articles with error ${response.status}`
-      );
-      return;
-    }
-
-    let data = response.data;
-    for (const article of data["articles"]) {
-      log.push(article);
-    }
-    url = data.next_page;
-  }
-
   let jsonLogs = JSON.stringify(log);
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, "0");
-  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-  var yyyy = today.getFullYear();
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, "0");
+  let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  let yyyy = today.getFullYear();
 
   today = mm + "-" + dd + "-" + yyyy;
   fs.writeFile(`backups/Backup-${today}.json`, jsonLogs, (err) => {
@@ -148,4 +65,4 @@ async function exportMso() {
   });
 }
 
-exportTwilioFlex();
+exportArticles();
