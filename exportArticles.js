4a0edc95cf1e744bd93129dace6d5ea4d2e56540
chris-perficient <christopher.mcardle@perficient.com>
2022-03-28 15:23:11 -0500

    Split clarity into two separate function to honor rate limit

--- exportArticles.js
+++ exportArticles.js
@@ -8,44 +8,45 @@
 require("dotenv").config();
 const fs = require("fs");
 const axios = require("axios");
-const download = require("image-downloader");
 const token = process.env.ZENDESK_TOKEN;
 const encoded = Buffer.from(
-  "lara.keenan@perficient.com/token:" + token
+  "christopher.mcardle@perficient.com/token:" + token
 ).toString("base64");
 
 let log = [];
 
 async function downloadImage(brand, articleId) {
-  // console.log("URLLLLL", brand);
   let url = `https://${brand}.zendesk.com/api/v2/help_center/articles/${articleId}/attachments`;
-  const response = await axios
-    .get(url, {
-      headers: { Authorization: `Basic ${encoded}` },
-    })
-    .catch((err) => console.log("ERRORRR", err));
+  try {
+    const response = await axios
+      .get(url, {
+        headers: { Authorization: `Basic ${encoded}` },
+      })
+      .catch((err) => console.log("ERRORRR", err));
 
-  // console.log("RESPONSEEEE", response.data.article_attachments);
-  let attachmentsArray = response.data.article_attachments;
-  if (attachmentsArray.length >= 1) {
-    for (let i = 0; i < attachmentsArray.length; i++) {
-      let attachments_url = attachmentsArray[i].content_url
-      axios
-        .get(attachments_url, {
-          method: "GET",
-          responseType: "stream",
-          headers: { Authorization: `Basic ${encoded}` },
-        })
-        .then((e) => {
-          const path = `./backupImages/${articleId}-(${i}).png`;
-          const writer = fs.createWriteStream(path);
-          e.data.pipe(writer);
-          return new Promise((resolve, reject) => {
-            writer.on("finish", resolve);
-            writer.on("error", reject);
+    let attachmentsArray = response.data.article_attachments;
+    if (attachmentsArray.length >= 1) {
+      for (let i = 0; i < attachmentsArray.length; i++) {
+        let attachments_url = attachmentsArray[i].content_url;
+        axios
+          .get(attachments_url, {
+            method: "GET",
+            responseType: "stream",
+            headers: { Authorization: `Basic ${encoded}` },
+          })
+          .then((e) => {
+            const path = `./backupImages/${articleId}-(${i}).png`;
+            const writer = fs.createWriteStream(path);
+            e.data.pipe(writer);
+            return new Promise((resolve, reject) => {
+              writer.on("finish", resolve);
+              writer.on("error", reject);
+            });
           });
-        });
+      }
     }
+  } catch {
+    (e) => console.log("ERRORRR", e);
   }
 }
 
@@ -53,92 +54,103 @@ async function exportTwilioFlex() {
   let brand = "perficient";
   let url = `https://${brand}.zendesk.com/api/v2/help_center/en-us/articles`;
   let response;
-  while (url) {
-    response = await axios.get(url, {
-      headers: { Authorization: `Basic ${encoded}` },
-    });
+  try {
+    while (url) {
+      response = await axios.get(url, {
+        headers: { Authorization: `Basic ${encoded}` },
+      });
 
-    if (response.status != 200) {
-      console.log(
-        `Failed to retrieve Twilio Flex articles with error ${response.status}`
-      );
-      return;
+      if (response.status != 200) {
+        console.log(
+          `Failed to retrieve Twilio Flex articles with error ${response.status}`
+        );
+        return;
+      }
+
+      let data = response.data;
+      for (const article of data["articles"]) {
+        downloadImage(brand, article.id);
+        log.push(article);
+      }
+      url = data.next_page;
     }
-
-    let data = response.data;
-    for (const article of data["articles"]) {
-      await downloadImage(brand, article.id);
-      log.push(article);
-    }
-
-    url = data.next_page;
+  } catch {
+    (e) => console.log("ERROR IN TWILIO FLEX", e);
   }
-  exportTwilioFlex2();
+  console.log("FINISHED EXPORTING TWILIO FLEX");
+  setTimeout(exportAmazonConnect, 60000);
 }
 
-async function exportTwilioFlex2() {
+async function exportAmazonConnect() {
   let brand = "prftamazonconnect";
   let url = `https://${brand}.zendesk.com/api/v2/help_center/en-us/articles`;
   let response;
-  while (url) {
-    response = await axios.get(url, {
-      headers: { Authorization: `Basic ${encoded}` },
-    });
+  try {
+    while (url) {
+      response = await axios.get(url, {
+        headers: { Authorization: `Basic ${encoded}` },
+      });
 
-    if (response.status != 200) {
-      console.log(
-        `Failed to retrieve Twilio Flex articles with error ${response.status}`
-      );
-      return;
+      if (response.status != 200) {
+        console.log(
+          `Failed to retrieve Twilio Flex articles with error ${response.status}`
+        );
+        return;
+      }
+
+      let data = response.data;
+      for (const article of data["articles"]) {
+        downloadImage(brand, article.id);
+        log.push(article);
+      }
+      url = data.next_page;
     }
-
-    let data = response.data;
-    for (const article of data["articles"]) {
-      // console.log("ARTICLE", article);
-
-      downloadImage(brand, article.id);
-      log.push(article);
-    }
-    url = data.next_page;
+  } catch {
+    (e) => console.log("ERROR IN AMAZON CONNECT", e);
   }
-  exportTwilioFlex3();
+  console.log("FINISHED EXPORTING AMAZON CONNECT");
+  setTimeout(exportMso, 60000);
 }
 
-async function exportTwilioFlex3() {
+async function exportMso() {
   let brand = "prft-mso";
   let url = `https://${brand}.zendesk.com/api/v2/help_center/en-us/articles`;
   let response;
-  while (url) {
-    response = await axios.get(url, {
-      headers: { Authorization: `Basic ${encoded}` },
-    });
+  try {
+    while (url) {
+      response = await axios.get(url, {
+        headers: { Authorization: `Basic ${encoded}` },
+      }).catch((err) => console.log("ERROR IN MSO", err));
 
-    if (response.status != 200) {
-      console.log(
-        `Failed to retrieve Twilio Flex articles with error ${response.status}`
-      );
-      return;
+      if (response.status != 200) {
+        console.log(
+          `Failed to retrieve Twilio Flex articles with error ${response.status}`
+        );
+        return;
+      }
+
+      let data = response.data;
+      for (const article of data["articles"]) {
+        downloadImage(brand, article.id);
+        log.push(article);
+      }
+      url = data.next_page;
     }
-
-    let data = response.data;
-    for (const article of data["articles"]) {
-
-      downloadImage(brand, article.id);
-      log.push(article);
-    }
-    url = data.next_page;
+  } catch {
+    (e) => console.log("ERRORRR", e);
   }
-  exportTwilioFlex4();
+  console.log("FINISHED EXPORTING MSO");
+  setTimeout(exportConverge, 60000);
 }
 
-async function exportTwilioFlex4() {
+async function exportConverge() {
   let brand = "prft-converge";
   let url = `https://${brand}.zendesk.com/api/v2/help_center/en-us/articles`;
   let response;
   while (url) {
-    response = await axios.get(url, {
-      headers: { Authorization: `Basic ${encoded}` },
-    });
+      response = await axios.get(url, {
+        headers: { Authorization: `Basic ${encoded}` },
+      }).catch((err) => console.log("ERROR IN CONVERGE", err));
 
     if (response.status != 200) {
       console.log(
@@ -149,38 +161,58 @@ async function exportTwilioFlex4() {
 
     let data = response.data;
     for (const article of data["articles"]) {
-
       downloadImage(brand, article.id);
       log.push(article);
     }
     url = data.next_page;
   }
-  exportTwilioFlex5();
+  console.log("FINISHED EXPORTING CONVERGE");
+  setTimeout(exportClarityConnect, 60000);
 }
 
-async function exportTwilioFlex5() {
+async function exportClarityConnect() {
   let brand = "prft-cconnect";
+  let clarityArticles = [];
   let url = `https://${brand}.zendesk.com/api/v2/help_center/en-us/articles`;
   let response;
   while (url) {
-    response = await axios.get(url, {
-      headers: { Authorization: `Basic ${encoded}` },
-    });
-
+    response = await axios
+      .get(url, {
+        headers: { Authorization: `Basic ${encoded}` },
+      }).catch((err) => console.log("ERROR IN CLARITY CONNECT", err));
     if (response.status != 200) {
       console.log(
         `Failed to retrieve Twilio Flex articles with error ${response.status}`
       );
       return;
     }
-
     let data = response.data;
     for (const article of data["articles"]) {
-
-      downloadImage(brand, article.id);
+      // setTimeout(() => {  console.log("World!"); }, 1000);
+      // downloadImage(brand, article.id);
+      clarityArticles.push(article);
       log.push(article);
     }
     url = data.next_page;
+  }
+
+  const half = Math.ceil(clarityArticles.length / 2);
+  const firstHalf = clarityArticles.slice(0, half);
+  const secondHalf = clarityArticles.slice(-half);
+
+  for (const article of firstHalf) {
+    downloadImage(brand, article.id);
+  }
+
+  setTimeout(exportClarityConnect2, 60000, secondHalf);
+
+}
+
+async function exportClarityConnect2(secondHalf) {
+  let brand = "prft-cconnect";
+
+  for (const article of secondHalf) {
+    downloadImage(brand, article.id);
   }
 
   let jsonLogs = JSON.stringify(log);
@@ -190,12 +222,17 @@ async function exportTwilioFlex5() {
   let yyyy = today.getFullYear();
 
   today = mm + "-" + dd + "-" + yyyy;
+  console.log("FINISHED EXPORTING CLARITY CONNECT");
+
   fs.writeFile(`backups/Backup-${today}.json`, jsonLogs, (err) => {
     if (err) console.log(err);
     else {
       console.log("File written successfully\n");
     }
+    return;
   });
+
+
 }
 
 exportTwilioFlex();
